0354843c139c5a13d9699b3e9adef073cd4efe23
Mu-Gee <61751306+Mu-Gee@users.noreply.github.com>
2024-04-02 18:51:04 +0300

    configuring debugging and release modes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-/*#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}*/
 
 mod auth;
-
 use auth::User;
-
 use std::io;
+
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
 
 // Function to prompt the user for input
 fn prompt(message: &str) -> String {
@@ -23,11 +23,11 @@ fn prompt(message: &str) -> String {
 }
 
 fn main() {
-    /*tauri::Builder::default()
+    tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-*/
+
 
     // Get username and password from the user
      // Get username from the user
